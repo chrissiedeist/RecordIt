80fffaccbcbf3caa8238eef79dbf6d7e473dcec1
Chrissie Deist <chrissie.deist@gmail.com>
2014-03-31 13:03:29 -0700

    Create active_record_lite.rb
    Require this file to use this library

---
+++ lib/active_record_lite.rb
@@ -0,0 +1,5 @@
+require_relative './active_record_lite/04_associatable'
+require_relative './active_record_lite/db_connection'
+require_relative './active_record_lite/01_mass_object'
+require_relative './active_record_lite/03_searchable'
+require_relative './active_record_lite/02_sql_object'
