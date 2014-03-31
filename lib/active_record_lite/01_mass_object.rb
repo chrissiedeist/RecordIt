9dd491e35c43239d813fdc6ec4fa4bd73d53189d
Chrissie Deist <chrissie.deist@gmail.com>
2014-03-31 13:01:39 -0700

    Update 01_mass_object.rb

--- lib/active_record_lite/01_mass_object.rb
+++ lib/active_record_lite/01_mass_object.rb
@@ -1,16 +1,32 @@
-# deprecated for Rails 4
-# require_relative '00_attr_accessor_object.rb'
-#
-# class MassObject < AttrAccessorObject
-#   def self.my_attr_accessible(*new_attributes)
-#     # ...
-#   end
-#
-#   def self.attributes
-#     # ...
-#   end
-#
-#   def initialize(params = {})
-#     # ...
-#   end
-# end
+class MassObject
+  def self.my_attr_accessible(*attributes)
+		attributes.each do |attribute|
+			define_method("#{attribute}=") do |value|
+				instance_variable_set("@#{attribute}", value)
+			end
+			define_method(attribute) do
+				instance_variable_get("@#{attribute}")
+			end
+		end
+
+		@attributes = attributes
+  end
+
+  def self.attributes
+  	@attributes
+  end
+
+  def self.parse_all(results)
+  end
+
+  def initialize(params = {})
+  	params.each_pair do |attr_name, value|
+  		if self.class.attributes.include?(attr_name.to_sym)
+  			send("#{attr_name}=", value)
+  		else
+  			raise "mass assignment to unregistered attribute #{attr_name}"
+  		end
+  	end
+
+  end
+end
