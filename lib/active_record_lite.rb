e42a31fb46efb198b326a55978d31fa8a2dcf909
Chrissie Deist <chrissie.deist@gmail.com>
2014-03-31 20:44:05 -0700

    Update active_record_lite.rb

--- lib/active_record_lite.rb
+++ lib/active_record_lite.rb
@@ -1,5 +1,4 @@
 require_relative './active_record_lite/04_associatable'
 require_relative './active_record_lite/db_connection'
-require_relative './active_record_lite/01_mass_object'
 require_relative './active_record_lite/03_searchable'
 require_relative './active_record_lite/02_sql_object'
